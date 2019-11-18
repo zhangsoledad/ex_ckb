c8443f93e9e0caf26afb3eb11a51f3887b13ec2c
zhangsoledad <787953403@qq.com>
2019-11-18 16:13:39 +0800

    init cm

---
+++ lib/ex_ckb.ex
@@ -0,0 +1,18 @@
+defmodule ExCkb do
+  @moduledoc """
+  Documentation for ExCkb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCkb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_ckb_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCkbTest do
+  use ExUnit.Case
+  doctest ExCkb
+
+  test "greets the world" do
+    assert ExCkb.hello() == :world
+  end
+end
